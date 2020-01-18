187a084bccec521b7032ce57cc50684efec77170
Piotr Mol <piotr.mol@iteo.com>
2020-01-18 14:06:53 +0100

    Initial Commit

---
+++ HumanResources/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HumanResources
+//
+//  Created by Piotr Mol on 18/01/2020.
+//  Copyright © 2020 Piotr Mol. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
